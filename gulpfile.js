6a1bd2877204132c5c6752a0635ea99daf55bcc8
Anna_Shant <anna.lunatic.t@gmail.com>
2019-07-09 01:50:02 +0500

    Окончательная верстка и модальные окна

--- gulpfile.js
+++ gulpfile.js
@@ -1,95 +1,37 @@
 "use strict";
 
 var gulp = require("gulp");
-var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
-var minify = require("gulp-csso");
-var rename = require("gulp-rename");
-var imagemin = require("gulp-imagemin");
-var posthtml = require("gulp-posthtml");
-var include = require("posthtml-include");
-var run = require("run-sequence");
-var del = require("del");
-var uglify = require("gulp-uglify");
 
-gulp.task("images", function (done) {
-  gulp.src("source/img/**/*.{png,jpg,svg}")
-    .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true}),
-      imagemin.svgo()
-    ]))
-    .pipe(gulp.dest("build/img"));
-    done();
-});
-
-gulp.task("html", function (done) {
-  gulp.src("source/*.html")
-    .pipe(posthtml([
-      include()
-    ]))
-    .pipe(gulp.dest("build"));
-    done();
-});
-
-gulp.task("copy", function (done) {
-   gulp.src([
-      "source/fonts/**/*.{woff,woff2}",
-      "source/img/**",
-      "source/pic/**",
-      "source/js/**",
-      "source/vendor/**",
-  ], {
-    base: "source"
-  })
-    .pipe(gulp.dest("build"));
-    done();
-});
-
-
-gulp.task("clean", function (done) {
-  del("build");
-  done();
-});
-
-gulp.task("style", function(done) {
-  gulp.src("source/sass/style.scss")
+gulp.task("css", function () {
+  return gulp.src("source/sass/style.scss")
     .pipe(plumber())
+    .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("build/css"))
-    .pipe(minify())
-    .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("build/css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
-    done();
 });
 
-gulp.task("compress", function(done) {
-  gulp.src("source/js/*.js")
-    .pipe(uglify())
-    .pipe(rename({suffix: ".min"}))
-    .pipe(gulp.dest("build/js"));
-    done();
-});
-
-gulp.task("server", function(done) {
+gulp.task("server", function () {
   server.init({
-    server: "build/",
+    server: "source/",
     notify: false,
     open: true,
     cors: true,
-    ui: false,
-    done
+    ui: false
   });
 
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.parallel("style"));
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task("start", gulp.series("clean", "copy", "style", "images", "html", "compress", "server"));
+gulp.task("start", gulp.series("css", "server"));
--- source/js/script.js
+++ source/js/script.js
@@ -8,18 +8,20 @@ $(document).ready(function(){
      infinite: false,
      speed: 300,
      slidesToShow: 1,
-     adaptiveHeight: true,
-     variableWidth: true,
      swipe: false,
      draggable: false,
+     focusOnSelect: false,
+     centerMode: false,
      responsive: [
       {
       breakpoint: 1366,
       settings: {
         swipe: true,
         draggable: true,
-        slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        focusOnSelect: false,
+        centerMode: false
       }
     },
       {
@@ -28,7 +30,9 @@ $(document).ready(function(){
           swipe: true,
           draggable: true,
           slidesToShow: 2,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          focusOnSelect: false,
+          centerMode: false
         }
       },
       {
@@ -37,7 +41,9 @@ $(document).ready(function(){
           swipe: true,
           draggable: true,
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          focusOnSelect: true,
+          centerMode: true
         }
       },
       {
@@ -46,12 +52,65 @@ $(document).ready(function(){
           swipe: true,
           draggable: true,
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          focusOnSelect: true,
+          centerMode: true
         }
       }
     ]
   })
- });
+});
+
+
+ //Слайдер в блоке "Услуги"
+$(document).ready(function(){
+  $('#work').slick({
+     dots: false,
+     arrows: false,
+     infinite: false,
+     speed: 100,
+     slidesToShow: 1,
+     swipe: false,
+     draggable: false,
+     focusOnSelect: false,
+     centerMode: false,
+     responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          swipe: true,
+          draggable: true,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          focusOnSelect: false,
+          centerMode: false
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          swipe: true,
+          draggable: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          focusOnSelect: true,
+          centerMode: true
+        }
+      },
+      {
+        breakpoint: 360,
+        settings: {
+          swipe: true,
+          draggable: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          focusOnSelect: true,
+          centerMode: true
+        }
+      }
+    ]
+  })
+});
 
 //Валидация форм
 $('.input-phone').on('input invalid', function() {
@@ -65,7 +124,10 @@ $('.input-phone').on('input invalid', function() {
   if (this.validity.patternMismatch) {
     this.setCustomValidity('Данные введены не верно, укажите номер телефона: +7 909 000 33 33')
   }
-})
+});
+
+//Маска ввода телефона
+$(".input-phone").mask("+7 (999) 999-99-99");
 
 //Модальное окно с формой
 var modalForm = document.getElementById('modalForm');
@@ -97,19 +159,20 @@ function closeForm() {
 var modal1 = document.getElementById('modalConfirmation');
 var modalPresent = document.getElementById('modalPresent');
 var myBtn1 = document.getElementsByClassName('btn__opened-1');
-var BtnPresent = document.getElementsByClassName('btn__present');
+var BtnPresent = document.getElementsByClassName('btn__anketa');
 var close1 = document.getElementsByClassName('modal__close-1')[0];
 var closeCheck = document.getElementsByClassName('btn__modal')[0];
 var closePresent = document.getElementsByClassName('modal__close-3')[0];
+var closePresent1 = document.getElementsByClassName('modal-confirmation-close')[0];
 var over1 = document.getElementsByClassName('modal-form__overlay')[0];
 
-for (var i = 0; i < myBtn1.length; i++) {
-  myBtn1[i].addEventListener('click', function() {
-    openModal1Window();
-  });
-}
+// for (var i = 0; i < myBtn1.length; i++) {
+//   myBtn1[i].addEventListener('click', function() {
+//     openModal1Window();
+//   });
+// }
 
-for (var i = 0; i < myBtn1.length; i++) {
+for (var i = 0; i < BtnPresent.length; i++) {
   BtnPresent[i].addEventListener('click', function() {
     openModal2Window();
   });
@@ -118,6 +181,7 @@ for (var i = 0; i < myBtn1.length; i++) {
 close1.addEventListener('click', closeModal1Window);
 closeCheck.addEventListener('click', closeModal2Window);
 closePresent.addEventListener('click', closeModal3Window);
+closePresent1.addEventListener('click', closeModal4Window);
 
 function openModal1Window() {
   modal1.style.display = 'block';
@@ -144,3 +208,127 @@ function closeModal3Window() {
   over1.style.display = 'none';
   $('body').css('overflow', 'auto');
 }
+
+function closeModal4Window() {
+  modalPresent.style.display = 'none';
+  over1.style.display = 'none';
+  $('body').css('overflow', 'auto');
+}
+
+//Модальное окно 1
+var modalForm1 = document.getElementById('modalForm1');
+var formBtn1 = document.getElementsByClassName('open-1');
+var formClose1 = document.getElementsByClassName('modal-form__close-1')[0];
+var formOver1 = document.getElementsByClassName('modal-form__overlay')[0];
+
+for (var i = 0; i < formBtn1.length; i++) {
+  formBtn1[i].addEventListener('click', function() {
+    openForm1();
+  });
+}
+
+formClose1.addEventListener('click', closeForm1);
+
+function openForm1() {
+  modalForm1.style.display = 'block';
+  formOver1.style.display = 'block';
+  $('body').css('overflow', 'hidden');
+}
+
+function closeForm1() {
+  modalForm1.style.display = 'none';
+  formOver1.style.display = 'none';
+  $('body').css('overflow', 'auto');
+}
+
+//Модальное окно 2
+var modalForm2 = document.getElementById('modalForm2');
+var formBtn2 = document.getElementsByClassName('open-2');
+var formClose2 = document.getElementsByClassName('modal-form__close-2')[0];
+var formOver2 = document.getElementsByClassName('modal-form__overlay')[0];
+
+for (var i = 0; i < formBtn2.length; i++) {
+  formBtn2[i].addEventListener('click', function() {
+    openForm2();
+  });
+}
+
+formClose2.addEventListener('click', closeForm2);
+
+function openForm2() {
+  modalForm2.style.display = 'block';
+  formOver2.style.display = 'block';
+  $('body').css('overflow', 'hidden');
+}
+
+function closeForm2() {
+  modalForm2.style.display = 'none';
+  formOver2.style.display = 'none';
+  $('body').css('overflow', 'auto');
+}
+
+//Открытие/закрытие списка
+function showHide(element_id) {
+  if (document.getElementById(element_id)) {
+    var obj = document.getElementById(element_id);
+  if (obj.style.display != "block") {
+    obj.style.display = "block";}
+  else obj.style.display = "none"; }
+  else alert("Элемент с id: " + element_id + " не найден!");
+}
+
+if ($('html[lang="ru"]').length !== 0) {
+  $.ajax({
+      url: 'https://docs.google.com/document/d/e/2PACX-1vTCfO8_RVMmIfjqg21FK2IGWpcyrc8k_63IEjaB_9hhWa-ipswOEp9Pzkh2DNTvd0NZUzHRY5k2lUqb/pub',
+      success: function(data) {
+          var htmlTag = document.createElement('html');
+              htmlTag.innerHTML = data;
+          var $text = $(htmlTag).find('body').children();
+          $text.find('style').remove();
+          $('#mfPrivacyPolicy .modal-form__content').empty().append($text);
+      }
+  });
+}
+
+$('#mfPrivacyPolicy .modal-form__close').on('click', function() {
+  var $modal = $(this).parents('#mfPrivacyPolicy');
+  var $overlay = $modal.prev();
+
+  $overlay.hide();
+  $modal.hide();
+
+  $('body').css('overflow', 'auto');
+});
+
+$('a[href="#privacy-policy"]').on('click', function(event) {
+  var $modal = $('#mfPrivacyPolicy');
+  var $overlay = $modal.prev();
+
+  $overlay.show();
+  $modal.show();
+
+  $('body').css('overflow', 'hidden');
+
+  event.preventDefault();
+  event.stopImmediatePropagation();
+});
+
+$('form').on('submit', function() {
+  var isValid = this.checkValidity();
+
+  if (isValid) {
+    var $button = $(this).find('button[type="submit"]');
+    $button.attr('disabled', true);
+    $.post(
+      'controller.php',
+      $(this).serialize(),
+      function(response) {
+        $('.modal-form').hide();
+        $button.attr('disabled', false);
+        openModal1Window();
+      }
+    );
+  }
+
+  return false;
+});
